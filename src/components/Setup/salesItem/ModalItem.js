905e269859cfd8d9e21bf8c45d647e41cfc70601
Mario <mario.attieh.2@gmail.com>
2021-08-18 13:08:40 +0300

    Trying new modal

---
+++ src/components/Setup/salesItem/ModalItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../../../styles/Items.css'
+
+const ModalItem = () => {
+    return(
+        <div className='modal-item'>
+            <div className='modal-item-header'>
+                Add new item
+            </div>
+            <div className='modal-item-body'>
+
+            </div>
+            <div className='modal-item-footer'>
+
+            </div>
+        </div>
+    )
+}
+
+export default ModalItem
